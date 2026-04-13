b1e14bcdbba598336c5e84bffcb8da0dfd69fcbb
Adithya <147333101+adithya-1010010@users.noreply.github.com>
2026-04-13 16:43:43 +0000

    added new basics file(lesson1)

---
+++ 01_basics/keyword.js
@@ -0,0 +1,23 @@
+const pass=1221
+let userName="adithya";
+var email="adithya@gmail.com"       
+
+/*
+var → only cares about functions
+let/const → care about { blocks }
+*/
+
+
+console.table([pass,userName,email])
+//to print more than one value at a time
+
+
+
+const obj = {
+  name: "Adithya",
+  age: 18
+};
+console.table(obj);
+// 👉 Shows key-value pairs as table.
+
+
